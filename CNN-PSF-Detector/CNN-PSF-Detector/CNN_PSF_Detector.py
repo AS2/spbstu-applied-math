b251dfc07db6b9ae70340b801137d83bd0455093
AS2 <alex.sachuk@yandex.ru>
2022-07-05 16:17:03 +0300

    First commit of SPbSTU's summer practice
    Commited:
    - 'DataSetMaker' module: provides data sets generating for CNN in usefull format (needs testing)
    - 'CNN-PSF-Detector' module: provides generating PSF from image and CNN learning from modules (not ready in any approximation)

---
+++ CNN-PSF-Detector/CNN-PSF-Detector/CNN_PSF_Detector.py
@@ -0,0 +1,13 @@
+from ModelReader import ModelReader
+from CNNModel import CNNModel
+
+def run(modelName):
+    mr = ModelReader()
+    if not mr.ReadModel(modelName):
+        print("Model reading failure. Stop working.")
+        return
+
+    return
+
+if __name__ == "__main__":
+    run("./datasets/testModel.model")
